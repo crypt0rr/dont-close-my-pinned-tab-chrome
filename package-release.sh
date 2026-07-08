bbf9639721c67475ec4eab44c06857fe8be97874
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2026-07-08 18:06:00 +0200

    Add package release script for version 0.3.0

---
+++ package-release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+VERSION="0.3.0"
+PACKAGE_NAME="dont-close-my-pinned-tab-chrome-v${VERSION}.zip"
+
+mkdir -p dist
+rm -f "dist/${PACKAGE_NAME}"
+
+zip -r "dist/${PACKAGE_NAME}" \
+  manifest.json \
+  background.js \
+  options.html \
+  options.css \
+  options.js \
+  icons \
+  README.md \
+  PRIVACY.md \
+  STORE_LISTING.md \
+  LICENSE \
+  -x "*.DS_Store"
+
+echo "Created dist/${PACKAGE_NAME}"
